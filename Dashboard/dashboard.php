371ef47bf93e16c098d9d241b0a0f7eaa58f6a05
Tettey Divine <98411773+OPOKUSIAWEUGENE@users.noreply.github.com>
2022-11-03 11:21:16 +0000

    dashboard_js

--- Dashboard/dashboard.php
+++ Dashboard/dashboard.php
@@ -49,7 +49,9 @@
         Revenue last 7 days
     </div>
    <div class="main_chart">
-    
+    <div class="chart">
+        
+    </div>
    </div>  
     </div>
     <div class="main_bottom">
@@ -57,7 +59,7 @@
         Best Sellers
         </div>
         <div class="main_table">
-<table>
+<table id="products_table">
     <thead>
         <tr>
             <th>
@@ -76,21 +78,7 @@
         </tr>
     </thead>
     <tbody>
-        <tr>
-            <td>Item 1</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Item 2</td>
-            <td></td>
-            <td></td>
-        </tr>
-        <tr>
-            <td>Item 3</td>
-            <td></td>
-            <td></td>
-        </tr>
+       
     </tbody>
 </table>
         </div>
@@ -101,3 +89,47 @@
 
     </body>
 </html>
+
+<script>
+
+async function Dashboard(){
+let access_token=localStorage.getItem('access_token');
+const response= await fetch('https://freddy.codesubmit.io/dashboard',{
+method:"Get",
+headers:{"Content-type":"Application/json", 'Authorization':'Bearer '+access_token},
+})
+status= response.status;
+data= await response.json();
+
+if(status==200)
+{
+ let tabledata=data.dashboard.bestsellers;
+let myTable=document.getElementById('products_table').getElementsByTagName('tbody')[0];
+
+
+tabledata.forEach(myFunction);
+
+function myFunction(item){
+
+let row=myTable.insertRow();
+let cell1=row.insertCell(0);
+let cell2=row.insertCell(1);
+let cell3=row.insertCell(2);
+let cell4=row.insertCell(3);
+
+
+cell1.innerHTML=item.product.name;
+cell2.innerHTML="";
+cell3.innerHTML=item.units;
+cell4.innerHTML=item.revenue;
+}
+}
+else{
+
+console.log(data.msg);
+}
+
+}
+Dashboard();
+
+</script>
